93b8b410b125b9e6bf2cba80661917431bfede51
vinisebold <vinicius.sebold05@gmail.com>
2025-06-24 16:04:56 -0300

    add: desafio final

---
+++ desafio-final/README.md
@@ -0,0 +1,50 @@
+# 📘 Desafio Final – Fundamentos de Java
+
+Este projeto foi desenvolvido como parte do **Desafio Final** do curso **Entra21 – Fundamentos de Java**.
+
+---
+
+## Contextualização
+
+Uma empresa de consultoria está desenvolvendo um protótipo em linha de comando para monitorar o consumo mensal de energia elétrica em residências. Com base nesses dados, o sistema apresente:
+
+- O **consumo total** do período;
+- O **mês de maior e menor consumo**;
+- A **média de consumo**;
+- Uma **classificação do perfil de consumo** (baixo, moderado ou elevado).
+
+A aplicação será um protótipo de **linha de comando**, com foco em simular a lógica e o comportamento do sistema antes de ser integrado a uma solução com interface gráfica ou banco de dados.
+
+---
+
+## Requisitos do Programa
+
+### Desenvolver um programa que:
+
+- Solicite ao usuário os valores de **consumo mensal (kWh)** durante os **12 meses do ano**;
+- Armazene os dados em um **vetor** de tamanho 12;
+- Utilize **funções específicas** para:
+    - Calcular o **consumo total**;
+    - Calcular a **média mensal**;
+    - Identificar o **mês com maior e menor consumo**;
+    - Classificar o consumo mensal como:
+        - Até 150 kWh → **Baixo**
+        - De 151 a 300 kWh → **Moderado**
+        - Acima de 300 kWh → **Elevado**
+- Apresente um **relatório final completo e organizado** no terminal;
+- Utilize **boas práticas de codificação**, como nomes claros, indentação adequada e comentários explicativos;
+- Modularize o código, utilizando **funções além do `main()`**.
+
+---
+
+## 📥 Exemplo de Entradas (Input)
+
+<img src="resources/input.png" alt="Output 1" width="450" />
+
+---
+
+## 📤 Resultados Gerados (Output)
+
+<img src="resources/output.png" alt="Output 1" width="500" />
+
+
--- desafio-final/desafiofinal/Main.java
+++ desafio-final/desafiofinal/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         // Declaração de vetor
@@ -11,20 +12,38 @@ public class Main {
         String[] vetorMeses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
 
         // Entrada de dados
-        System.out.println("Informe o consumo mensal (kWh):");
+        System.out.println("=== Registro de Consumo Mensal (kWh) ===");
 
         for (int i = 0; i < vetorMeses.length; i++) {
-            System.out.print(vetorMeses[i] + ": ");
+            System.out.printf("→ %s: ", vetorMeses[i]);
             vetorValores[i] = sc.nextDouble();
         }
 
-        //Output
-        System.out.println("Consumo total: " + calcularConsumoTotal(vetorValores));
+        // Chamada de funções e Atribuições de valores
+        double total = calcularConsumoTotal(vetorValores);
+        double media = calcularMediaTotal(vetorValores, total);
+        int indiceMaior = verificarMaior(vetorValores);
+        int indiceMenor = verificarMenor(vetorValores);
+        String perfilConsumo = classificarConsumo(media);
+
+        // Output
+        imprimirRelatorio(vetorValores, vetorMeses, total, media, indiceMaior, indiceMenor, perfilConsumo);
 
 
         // Fecha Scanner
         sc.close();
     }
+
+    public static void imprimirRelatorio(Double[] pVetorValores, String[] pVetorMeses, double pTotal, double pMedia, int pIndiceMaior, int pIndiceMenor, String pPerfilConsumo) {
+        System.out.println("\n======== RELATÓRIO ========");
+        System.out.printf("🔌 Consumo Total: %.1f kWh%n", pTotal);
+        System.out.printf("📊 Média Mensal de Consumo: %.1f kWh/mês%n", pMedia);
+        System.out.printf("🔋 Mês de Maior Consumo: %s (%.1f kWh)%n", pVetorMeses[pIndiceMaior], pVetorValores[pIndiceMaior]);
+        System.out.printf("🪫 Mês de Menor Consumo: %s (%.1f kWh)%n", pVetorMeses[pIndiceMenor], pVetorValores[pIndiceMenor]);
+        System.out.printf("👤 Perfil de Consumo: %s%n", pPerfilConsumo);
+        System.out.println("============================");
+    }
+
 
     public static double calcularConsumoTotal(Double[] pVetorValores) {
 
@@ -37,14 +56,47 @@ public class Main {
         return consumoTotal;
     }
 
-    public static double calcularMediaTotal(Double[] pVetorValores) {
+    public static double calcularMediaTotal(Double[] pVetorValores, Double pTotal) {
+        return pTotal / pVetorValores.length;
+    }
 
-        double somaTotal = 0;
+    public static int verificarMaior(Double[] pVetorValor) {
 
-        for (int i = 0; i < pVetorValores.length; i++) {
-            
+        double maiorValor = Double.MIN_VALUE;
+        int indice = 0;
+
+        for (int i = 0; i < pVetorValor.length; i++) {
+            if (pVetorValor[i] > maiorValor) {
+                maiorValor = pVetorValor[i];
+                indice = i;
+            }
         }
 
-        return somaTotal;
+        return indice;
+    }
+
+    public static int verificarMenor(Double[] pVetorValor) {
+
+        double menorValor = Double.MAX_VALUE;
+        int indice = 0;
+
+        for (int i = 0; i < pVetorValor.length; i++) {
+            if (pVetorValor[i] < menorValor) {
+                menorValor = pVetorValor[i];
+                indice = i;
+            }
+        }
+
+        return indice;
+    }
+
+    public static String classificarConsumo(Double pMediaConsumo) {
+        if (pMediaConsumo <= 150) {
+            return "Baixo";
+        } else if (pMediaConsumo > 300) {
+            return "Elevado";
+        } else {
+            return "Moderado";
+        }
     }
 }
